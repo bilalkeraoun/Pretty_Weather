4fb5e2e2c8445d0cfeb25dacecb044970de7cfd3
bilal <bilalkeraoun@gmail.com>
2022-06-21 21:17:42 +0200

    Amélioration touche 3D

--- src/components/prettyweather/weather_widget/WeatherWidget.vue
+++ src/components/prettyweather/weather_widget/WeatherWidget.vue
@@ -9,19 +9,19 @@
 
 
         <!-- TITLE -->
-        <div class="title">
-            <h1>PRETTY WEATHER</h1>
+        <div class="title-flex">
+            <img src="./../../../assets/icons/coollogo_com-614625.png" class="title-logo">
+            <h1 class="title"></h1>
         </div>
 
         <!-- HEADER -->
         <div class="header_flex">
             <div class="header_item-flex">
-                <img :src="require('./../../../assets/icons/' + now.data.weather[0].icon + '.svg')" alt="logo du temps qu'il fait" class="header-logo-meteo">
-                <!--<img src="./../../../assets/icons/3d/6.png" alt="" class="header-logo-meteo">-->
+             <img src="./../../../assets/icons/3d/6.png" alt="" class="header-logo-meteo">
             </div>
             <div class="item-flex">
                 <p class="header_location">
-                    {{ location.data.name }}, 
+                    <span id="location">{{ location.data.name }}</span>,
                     {{ getFormatedDate(now.date) }} | {{ now.date.getHours() }}:{{ now.date.getMinutes() }}
                 </p>
                 <h2 class="header_temp">
@@ -36,27 +36,25 @@
             <div class="linear-gradient">
                 <!-- NEXT HOURS -->
                 <div class="box">
-                    <h5 class="box-title-padding">Prochaines Heures</h5>
-                    <div class="padding-hours" v-for="hour in next_hours" :key="hour">
-                            <div>
-                                <span class="next-hours_hours">{{ hour.date.getHours() }}</span>h
-                                <img :src="require('./../../../assets/icons/' + hour.data.weather[0].icon + '.svg')" alt="logo du temps qu'il fait" class="bloc-logo-meteo">
-                                <strong>{{ Math.round(hour.data.temp) }}°C</strong>
+                    <img src="./../../../assets/icons/prochaines-heures.png" class="box-3d-img">
+                    <div v-for="hour in next_hours" :key="hour">
+                            <div class="box-margin">
+                                {{ hour.date.getHours() }}h<br>
+                                <img :src="require('./../../../assets/icons/' + hour.data.weather[0].icon + '.svg')" alt="logo du temps qu'il fait" class="box-icones"><br>
+                                <strong>{{ Math.round(hour.data.temp) }}°C</strong><br>
                             </div>
                    </div>
                 </div>
                
         </div>
           <!-- NEXT DAYS -->
-                <div class="boxe">
-                    <h5 class="box-title-padding">Prochain Jours</h5>
-                    <div class="padding-days"  v-for="day in next_days" :key="day">
-                        <p class="next-hours_hours">
-                            {{ getFormatedDate(day.date) }}
-                        </p>
-                        <img :src="require('./../../../assets/icons/' + day.data.weather[0].icon + '.svg')" alt="logo du temps qu'il fait" class="bloc-logo-meteo">
+                <div class="box">
+                    <img src="./../../../assets/icons/prochaines-jours.png" class="box-3d-img">
+                    <div class="box-margin" v-for="day in next_days" :key="day">
+                           <span>{{ getFormatedDate(day.date) }}</span> <br>
+                        <img :src="require('./../../../assets/icons/' + day.data.weather[0].icon + '.svg')" alt="logo du temps qu'il fait" class="box-icones"><br>
                         <p>
-                           <strong>{{ Math.round(day.data.temp.min) }}°C | {{ Math.round(day.data.temp.max) }}°C </strong> 
+                           <strong>{{ Math.round(day.data.temp.min) }}°C | {{ Math.round(day.data.temp.max) }}°C</strong><br>
                         </p>
                     </div>
                     </div>
@@ -65,7 +63,6 @@
         <!-- FOOTER -->
         <footer>
             <div class="footer">
-            
             <div class="row">
              Pretty Weather Copyright © 2022 - All rights reserved || Designed By : Bilal.ux 
             </div>
@@ -154,102 +151,112 @@ export default {
     body {
         font-family: Arial, Helvetica, sans-serif;
         color:       #f1f1f1;
-        background:  rgb(48, 48, 48);
-    }
-    .container {
         background: linear-gradient(to bottom right, #5eb7b7, #5C9696);
-        border:     1px solid #f1f1f1;
-        width:      1100px;
-        margin:     100px auto 0;
     }
+   
 
-    /* TITLE */
-    .title h1 {
-      text-align:            center;
-      font-size:             2rem; 
-      font-family:           'Calvier';
-      padding-bottom:        1.5rem;
-      border-bottom:         1.5px solid;
+    /* TITLE  */
+   .title-flex{
+        display: flex;
+        justify-content: center;
+   }
+    .title-logo{
+        width: 40rem;
+        padding: 1.5rem;
     }
-    
 
     /* HEADER */
     .header_flex {
-    display:         flex;
-    justify-content: center;
-    align-items:     center;
-    margin: 0rem;
+        display:         flex;
+        justify-content: center;
+        align-items:     center;
     }
     .header-logo-meteo {
-    width: 40rem;
+        margin: 2rem;
+        width: 50rem;
     }
     .header_temp {
-    font-size: 3.5rem;
+    font-size: 6rem;
     }
     .header_location {
-    font-size: 1.5rem;
+    font-size: 2.5rem;
+    }
+    #location {
+        font-size: 3.5rem;
     }
 
-
-    /* NEXT HOURS | DAYS */
+    /* BOX */
     .box {
         text-align:    center;
         display:       flex;
         align-items:   center;
-        font-size:     1.2rem;
-        padding:       1rem;
-        padding-top:   4rem;
-        padding-bottom: 4rem;
+        font-size:     2rem;
+        padding:       4rem 1rem 4rem 1rem;
     }
-    .boxe {
-        text-align:    center;
-        display:       flex;
-        align-items:   center;
-        font-size:     1.2rem;
-        padding:       1rem;
-        padding-top:   2rem;
-        padding-bottom: 2rem;
+    .box-margin{
+        font-size: 2rem;
+        margin:    0 2rem;
     }
+    .box-icones{
+        width: 10rem;
+    }
+    .box-3d-img { 
+        width: 20rem;
+        padding: 0 7rem;
+    }
+    span { 
+        font-size: 1.5rem;
+    }
+
 
     .padding-hours {
       padding-right: 2rem;
     }
     .box-title-padding {
         padding-right: 3rem;
-        font-size: 1.5rem;
+        font-size: 2rem;
     }
 
     .padding-days {
-      padding-right: 2rem;
+        padding-right: 2rem;
     }
     .linear-gradient {
         background: linear-gradient(to bottom right, #5eb7b7, #5C9696);
     }
+    .prochaines-img {
+        width: 16rem;
+    }
 
     .next-hours_hours {
-    font-size: 1.1rem;
+        font-size: 1.5rem;
+        padding-left: 5rem;
     }
     .next-hours_deg {
-    font-size: 1.3rem;
+        font-size: 2rem;
     }
     .bloc-logo-meteo {
-    width: 5rem;
+        width: 5.5rem;
     }
+
+
+
+
 
     /* FOOTER */
     .footer {
         background:  rgb(249, 249, 249);
-        padding:     5px 0px;
+        padding:     3rem 0px;
         text-align:  center;
     }
     .footer .row {
         width:     100%;
         margin:    1% 0%;
         padding:   0.6% 0%;
-        color:     gray;
-        font-size: 0.8em;
+        color:     rgb(45, 45, 45);
+        font-size: 2rem;
     }
-    
+
+
     /* Animation chargement */
     .overlay-icone-chargement {
     position: absolute;
